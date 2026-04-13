e656156b38f69feabc592a8a771e1f23e0622ec1
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-04-13 00:41:40 -0500

    acaba mds

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './assets/css/css-estrutura/header.css'
 import './assets/css/css-estrutura/footer.css'
 import './assets/css/css-estrutura/cssmain/sobremim.css'
-import './assets/css/css-estrutura/cssmain/projetos.css'
+import './assets/css/css-estrutura/cssmain/projeto.css'
 import './assets/css/css-estrutura/cssmain/contato.css'
 
 import Header from './assets/estrutura/Header.jsx'
 import Footer from './assets/estrutura/Footer.jsx'
 //main
 import MainSobremim from './assets/estrutura/main/MainSobremim.jsx'
-import MainProjetos from './assets/estrutura/main/MainProjetos.jsx'
+import MainProjetos from './assets/estrutura/main/MainProjeto.jsx'
 import MainContato from './assets/estrutura/main/MainContato.jsx'
 
 function App() {
