7f9f526d64d8926c95c264cd61b1676a0d8405ac
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-04-18 22:10:16 -0500

    link e css

--- front-end/src/assets/estrutura/main/MainProjeto.jsx
+++ front-end/src/assets/estrutura/main/MainProjeto.jsx
@@ -5,24 +5,21 @@ const Projetos = () => {
             titulo: "Arcano Store",
             descricao: "O ponto de encontro para otakus que levam a cultura de anime a sério... mas nem tanto. Encontre aqui os melhores produtos, figures e acessórios direto do universo geek para a sua coleção. Se é de anime, está na Arcano!",
             techs: ["React", "PHP", "SQL"],
-            linkGithub: "https://github.com/RUBENSNETO0001/--Arcano_Store--.git",
-            linkDemo: "#"
+            linkGithub: "https://github.com/RUBENSNETO0001/--Arcano_Store--.git"
         },
         {
             id: 2,
             titulo: "Gerenciamento de Biblioteca",
             descricao: "Aplicação em Java para gerenciamento de bibliotecas, com funcionalidades CRUD para livros, empréstimos e usuários. Desenvolvida com foco em prática de programação orientada a objetos e manipulação de dados via console.",
             techs: ["Java", "SQL", "Swing"],
-            linkGithub: "https://github.com/falk-dev/java-library-manager.git",
-            linkDemo: "#"
+            linkGithub: "https://github.com/falk-dev/java-library-manager.git"
         },
         {
             id: 3,
             titulo: "Loja Belli Douceur",
             descricao: "Projeto de e-commerce para uma loja de doces, desenvolvido com HTML, CSS e JavaScript. O site apresenta um design atraente e responsivo, permitindo aos usuários navegar pelos produtos, adicionar itens ao carrinho e finalizar compras de forma intuitiva.",
             techs: ['HTML', 'CSS', 'JavaScript'],
-            linkGithub: "https://github.com/RUBENSNETO0001/--LojaBelliDouceur--",
-            linkDemo: "#"
+            linkGithub: "https://github.com/RUBENSNETO0001/--LojaBelliDouceur--"
         }
     ];
 
@@ -42,7 +39,6 @@ const Projetos = () => {
                             </div>
                             <div className="projeto-links">
                                 <a href={projeto.linkGithub} className="btn-card">GitHub</a>
-                                <a href={projeto.linkDemo} className="btn-card">Demo</a>
                             </div>
                         </div>
                     ))}
